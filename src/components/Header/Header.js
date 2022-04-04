5748b48b2299515be5fb36dd95626290907ad4e5
Tahmid Hasan <wdtahmid@gmail.com>
2022-04-04 19:14:25 +0600

    Custom link added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import CustomLink from '../hooks/CustomLink';
 
 const Header = () => {
     return (
         <div className='py-10'>
             <nav className='text-xl font-semibold uppercase flex flex-wrap gap-4 md:gap-10 justify-center'>
-                <Link to='/'>Home</Link>
-                <Link to='/reviews'>Reviews</Link>
-                <Link to='/dashboard'>Dashboard</Link>
-                <Link to='/blog'>Blog</Link>
-                <Link to='/about'>About</Link>
+                <CustomLink to='/'>Home</CustomLink>
+                <CustomLink to='/reviews'>Reviews</CustomLink>
+                <CustomLink to='/dashboard'>Dashboard</CustomLink>
+                <CustomLink to='/blog'>Blog</CustomLink>
+                <CustomLink to='/about'>About</CustomLink>
             </nav>
         </div>
     );
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>Page Not Found</h2>
+        <div className='h-screen flex flex-col items-center justify-center mt-5</br>'>
+            <h2 className='text-9xl font-bolder'>404</h2>
+            <h2 className='text-6xl font-bolder'>Page Not Found!!</h2>
         </div>
     );
 };
