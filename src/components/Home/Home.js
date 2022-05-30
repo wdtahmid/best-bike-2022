a1fd5c751ae035c89b63ad0c57141b6849f9767e
Tahmid Hasan <wdtahmid@gmail.com>
2022-05-30 10:19:48 +0600

    Add a text

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ const Home = () => {
                     <div className='w-full md:w-3/6 text-center md:text-left'>
                         <h1 className='text-5xl font-bold font-mono'>2014 EBR 1190RX</h1>
                         <p className='mt-3 font-serif uppercase font-semibold tracking-[.5em]'>Born to win the race!!!</p>
-                        <p className='my-10 font-serif font-semibold m-0 md:mr-20'>The Erik Buell Racing 1190RS is a sport bike that is manufactured by Erik Buell Racing in the United States. It was introduced in June 2011 for the 2012 model year. It is powered by a 1,190 cc (73 cu in), liquid-cooled V-twin engine that delivers 175 hp (130 kW) @ 9,750 rpm. Only 100 units were planned for 2011 production.</p>
+                        <p className='my-10 font-serif font-semibold m-0 md:mr-20'>Best Bike in 2022. The Erik Buell Racing 1190RS is a sport bike that is manufactured by Erik Buell Racing in the United States. It was introduced in June 2011 for the 2012 model year. It is powered by a 1,190 cc (73 cu in), liquid-cooled V-twin engine that delivers 175 hp (130 kW) @ 9,750 rpm. Only 100 units were planned for 2011 production.</p>
                         <button className='bg-indigo-800 hover:bg-indigo-900 text-white font-serif uppercase font-bold px-3 py-2 rounded'>live demo</button>
                     </div>
                     <div className='w-full md:w-3/6'>
